a21e4319f2c20f1d5653387951b00427d1139424
Anthony Weston <weston.anthony@gmail.com>
2018-02-10 12:43:46 -0500

    Completed the data generator multiplies two bytes

--- data_generator/data_generator.py
+++ data_generator/data_generator.py
@@ -7,3 +7,6 @@ def convert_to_binary_array(number: np.uint8):
 
 def add_op(number1: np.uint8, number2: np.uint8):
     return np.add(number1, number2)
+
+def multiply_op(number1: np.uint8, number2: np.uint8):
+    return np.multiply(number1, number2)
