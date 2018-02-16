741655f2cd4a9bb0621d03587213f01697d64ce1
Anthony Weston <weston.anthony@gmail.com>
2018-02-15 21:55:12 -0500

    Completed generate_training_example generates an example with the binary x values

--- generate_training_data.py
+++ generate_training_data.py
@@ -1,6 +1,6 @@
 import data_generator.data_generator as dg
 import os
-import numpy as np
+import tensorflow as tf
 
 
 UINT8_MAX = 255
@@ -10,8 +10,19 @@ OUTPUT_FOLDER = '{0}/training_data'.format(dirname)
 
 def generate_training_data():
     
-    for op in [dg.add_op, dg.multiply_op, dg.xor_op]:
-        continue
+    for op in [dg.add_op, dg.multiply_op, dg.xor_op]:         
+        for x in range(UINT8_MAX + 1):
+            for y in range(UINT8_MAX + 1):
+                generate_training_example(x, y)
+
+def generate_training_example(x, y):
+    x_values = dg.binary_array(x)
+    
+    training_example = tf.train.Example(features = tf.train.Features(feature = {
+        'x': tf.train.Feature(int64_list = tf.train.Int64List(value = x_values))
+        }))
+    
+    return training_example
 
 
 
--- test/test_generate_training_data.py
+++ test/test_generate_training_data.py
@@ -6,4 +6,8 @@ from generate_training_data import *
 
 class TestGenerateTrainingDataHelperFunctions(unittest.TestCase):
     
-    pass
+    def test_generate_training_example_generates_an_example_with_the_x_values(self):
+        subject = generate_training_example(101, 105)
+        
+        expected_value = dg.binary_array(101)
+        np.testing.assert_array_equal(expected_value, subject.features.feature['x'].int64_list.value)
