e4730da79337b88c4134f3ae0de4b5561ba38462
Anthony Weston <weston.anthony@gmail.com>
2018-02-25 10:29:45 -0500

    Completed generate_training_example generates an example with the z values, which are the result of applying an operation to x and y.

--- generate_training_data.py
+++ generate_training_data.py
@@ -1,6 +1,7 @@
 import data_generator.data_generator as dg
 import os
 import tensorflow as tf
+from data_generator import *
 
 
 UINT8_MAX = 255
@@ -13,15 +14,17 @@ def generate_training_data():
     for op in [dg.add_op, dg.multiply_op, dg.xor_op]:         
         for x in range(UINT8_MAX + 1):
             for y in range(UINT8_MAX + 1):
-                generate_training_example(x, y)
+                generate_training_example(x, y, op)
 
-def generate_training_example(x, y):
+def generate_training_example(x, y, op):
     x_values = dg.binary_array(x)
     y_values = dg.binary_array(y)
+    z_values = dg.binary_array(op(x, y))
     
     training_example = tf.train.Example(features = tf.train.Features(feature = {
         'x': tf.train.Feature(int64_list = tf.train.Int64List(value = x_values)),
-        'y': tf.train.Feature(int64_list = tf.train.Int64List(value = y_values))
+        'y': tf.train.Feature(int64_list = tf.train.Int64List(value = y_values)),
+        'z': tf.train.Feature(int64_list = tf.train.Int64List(value = z_values))
         }))
     
     return training_example
--- test/test_generate_training_data.py
+++ test/test_generate_training_data.py
@@ -7,13 +7,20 @@ from generate_training_data import *
 class TestGenerateTrainingDataHelperFunctions(unittest.TestCase):
     
     def test_generate_training_example_generates_an_example_with_the_x_values(self):
-        subject = generate_training_example(101, 105)
+        subject = generate_training_example(101, 105, add_op)
         
         expected_value = dg.binary_array(101)
         np.testing.assert_array_equal(expected_value, subject.features.feature['x'].int64_list.value)
         
     def test_generate_training_example_generates_an_example_with_the_y_values(self):
-        subject = generate_training_example(101, 105)
+        subject = generate_training_example(101, 105, add_op)
         
         expected_value = dg.binary_array(105)
         np.testing.assert_array_equal(expected_value, subject.features.feature['y'].int64_list.value)
+        
+    def test_generate_training_example_generates_an_example_with_the_result_of_applying_the_operation_to_x_and_y(self):
+        subject = generate_training_example(101, 105, add_op)
+        operation = add_op
+        
+        expected_value = dg.binary_array(operation(101, 105))
+        np.testing.assert_array_equal(expected_value, subject.features.feature['z'].int64_list.value)
