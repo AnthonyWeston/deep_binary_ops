2561933ca50c51e6b5f0d447bb21b4c640712287
Anthony Weston <weston.anthony@gmail.com>
2018-03-01 18:15:14 -0500

    Completed the data model parses the x values from the input file

---
+++ data.py
@@ -0,0 +1,26 @@
+import tensorflow as tf
+
+class Data:
+    
+    @staticmethod
+    def _parse(dataset: tf.data.TFRecordDataset):
+        features = {'x': tf.FixedLenFeature(shape = (8,), dtype = tf.int64)}
+        
+        parsed_features = tf.parse_single_example(dataset, features)
+        return parsed_features["x"]
+    
+    
+    
+    
+if __name__ == '__main__':
+    
+    data = tf.data.TFRecordDataset(['test/test_data.tfrecords'])
+    parsed_data = data.map(Data._parse)
+    
+    iterator = parsed_data.make_one_shot_iterator()
+
+    with tf.Session() as sess:
+        for i in range(4):
+            value = sess.run(iterator.get_next())
+            print(value)
+    
---
+++ test/test_data.py
@@ -0,0 +1,24 @@
+import unittest
+import tensorflow as tf
+from data import Data
+import numpy as np
+
+class TestData(unittest.TestCase):
+    
+    sess = None
+    
+    @classmethod
+    def setUpClass(cls):
+        sess = tf.Session()
+    
+    def setUp(self):
+        self.dataset = tf.data.TFRecordDataset(['test/test_data.tfrecords'])
+        
+    def test_the_data_model_parses_the_x_values_from_a_tfrecords_file(self):
+        subject = self.dataset.map(Data._parse)
+        
+        iterator = subject.make_one_shot_iterator()
+        first_row_x_values = iterator.get_next()
+        
+        expected_array = np.ndarray([0,0,0,0,0,0,0,0])
+        np.testing.assert_array_equal(expected_array, first_row_x_values)
