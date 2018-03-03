8961be16e1d9ab1458c4c46ea1953d297068e829
Anthony Weston <weston.anthony@gmail.com>
2018-03-03 10:44:46 -0500

    Completed the AbstractModelBuilder is created with a training batch size.

--- abstract_model_builder.py
+++ abstract_model_builder.py
@@ -2,6 +2,7 @@ from abc import ABC
 
 class AbstractModelBuilder(ABC):
     
-    def __init__(self, filename_list: list, training_size: int):
+    def __init__(self, filename_list: list, training_size: int, batch_size: int):
         self.filename_list = filename_list
         self.training_size = training_size
+        self.batch_size = batch_size
--- test/test_abstract_model.py
+++ test/test_abstract_model.py
@@ -9,10 +9,12 @@ class TestAbstractModelBuilder(unittest.TestCase):
     
     test_filename_list = [os.path.join(test_file_path, 'test_data.tfrecords')]
     training_size = 10
+    batch_size = 2
     
     def setUp(self):
         self.modelbuilder = AbstractModelBuilder(filename_list = TestAbstractModelBuilder.test_filename_list,
-            training_size = TestAbstractModelBuilder.training_size)
+            training_size = TestAbstractModelBuilder.training_size,
+            batch_size = TestAbstractModelBuilder.batch_size)
     
     def test_the_abstract_model_builder_is_created_with_a_list_of_data_filenames(self):
         subject = self.modelbuilder
@@ -25,3 +27,9 @@ class TestAbstractModelBuilder(unittest.TestCase):
         
         expected_training_size = TestAbstractModelBuilder.training_size
         self.assertEqual(expected_training_size, subject.training_size)
+        
+    def test_the_abstract_model_builder_is_created_with_a_training_batch_size(self):
+        subject = self.modelbuilder
+        
+        expected_batch_size = TestAbstractModelBuilder.batch_size
+        self.assertEqual(expected_batch_size, subject.batch_size)
