7787f193bb34c675ec8de0050f028c0d7745092e
Anthony Weston <weston.anthony@gmail.com>
2018-03-03 10:46:40 -0500

    Completed the AbstractModelBuilder is created with a random seed

--- abstract_model_builder.py
+++ abstract_model_builder.py
@@ -2,7 +2,8 @@ from abc import ABC
 
 class AbstractModelBuilder(ABC):
     
-    def __init__(self, filename_list: list, training_size: int, batch_size: int):
+    def __init__(self, filename_list: list, training_size: int, batch_size: int, seed: int):
         self.filename_list = filename_list
         self.training_size = training_size
         self.batch_size = batch_size
+        self.seed = seed
--- test/test_abstract_model.py
+++ test/test_abstract_model.py
@@ -10,11 +10,13 @@ class TestAbstractModelBuilder(unittest.TestCase):
     test_filename_list = [os.path.join(test_file_path, 'test_data.tfrecords')]
     training_size = 10
     batch_size = 2
+    seed = 1
     
     def setUp(self):
         self.modelbuilder = AbstractModelBuilder(filename_list = TestAbstractModelBuilder.test_filename_list,
             training_size = TestAbstractModelBuilder.training_size,
-            batch_size = TestAbstractModelBuilder.batch_size)
+            batch_size = TestAbstractModelBuilder.batch_size,
+            seed = TestAbstractModelBuilder.seed)
     
     def test_the_abstract_model_builder_is_created_with_a_list_of_data_filenames(self):
         subject = self.modelbuilder
@@ -33,3 +35,9 @@ class TestAbstractModelBuilder(unittest.TestCase):
         
         expected_batch_size = TestAbstractModelBuilder.batch_size
         self.assertEqual(expected_batch_size, subject.batch_size)
+        
+    def test_the_abstract_model_builder_is_created_with_a_random_seed(self):
+        subject = self.modelbuilder
+        
+        expected_seed = TestAbstractModelBuilder.seed
+        self.assertEqual(expected_seed, subject.seed)
